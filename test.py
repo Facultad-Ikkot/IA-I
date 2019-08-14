c5e0715af5434058ef876bd30c65420addb9cbc4
ignacio <ignacio1921@gmail.com>
2019-08-14 11:00:08 -0300

    up

--- test.py
+++ test.py
@@ -1,21 +1,3 @@
 
 import time
 
-
-
-def hola():
-    while True:
-        print("hola")
-        dor()
-
-def thinkAux(env):
-    if (self.prespective(env)):
-        self.suck(env)
-    env.print_environment()
-    if (self.periodo < 0):
-         return True 
-    time.sleep(self.sleepTime)
-
-
-
-hola()
