b5f2e530dee85b8e5be866e22faa81d8a2a0f786
ignacio <ignacio1921@gmail.com>
2019-08-27 16:17:55 -0300

    up

--- main.py
+++ main.py
@@ -11,15 +11,18 @@ agenMem = AgentConMeoria(env1)
 agenAlea = AgentAleatorio(env1)
 agenSin = AgentSinEstado(env1)
 
-
+c = 4
 while True:
-    # aux=agenSin.thinkSinMem(env1)
-    # aux=agenAlea.thinkAleatorio(env1)
-    aux = agenMem.think(env1)
-    # aux=agenMem.think2(env1)
+    if(c == 1):
+        aux = agenSin.thinkSinMem(env1)
+    if(c == 2):
+        aux = agenAlea.thinkAleatorio(env1)
+    if(c == 3):
+        aux = agenMem.think(env1)
+    if(c == 4):
+        aux = agenMem.think2(env1)
     if (aux == True):
         break
-
 
 print("################################ Final ################################")
 env1.print_environment()
