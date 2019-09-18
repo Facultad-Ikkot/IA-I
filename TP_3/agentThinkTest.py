98f58cb5162b68812464a2ed3a3948b852eacd5c
ikkot <ikkot>
2019-09-18 18:09:34 -0300

    up

---
+++ TP_3/agentThinkTest.py
@@ -0,0 +1,97 @@
+from random import randrange
+import time
+
+
+class AgentETest():
+    def __init__(self, env):
+        self.poblacion = []
+        self.poblacionRe = []
+        self.cont = 0
+
+    def comprobarFrontera(self, env, posActXTem, posActYTem):
+        state = False
+        padre = (posActXTem, posActYTem)
+        if (env.accept_action(posActXTem+1, posActYTem)):
+            aux = env.functioH(posActXTem+1, posActYTem)
+            pos = (posActXTem+1, posActYTem)
+            item = ((pos, padre), aux)
+            if (self.poblacionRe.count(item) == 0):
+                state = True
+                self.poblacion.append(item)
+        if (env.accept_action(posActXTem-1, posActYTem)):
+            aux = env.functioH(posActXTem-1, posActYTem)
+            pos = (posActXTem-1, posActYTem)
+            item = ((pos, padre), aux)
+            if (self.poblacionRe.count(item) == 0):
+                state = True
+                self.poblacion.append(item)
+        if (env.accept_action(posActXTem, posActYTem+1)):
+            aux = env.functioH(posActXTem, posActYTem+1)
+            pos = (posActXTem, posActYTem+1)
+            item = ((pos, padre), aux)
+            if (self.poblacionRe.count(item) == 0):
+                state = True
+                self.poblacion.append(item)
+        if (env.accept_action(posActXTem, posActYTem-1)):
+            aux = env.functioH(posActXTem, posActYTem-1)
+            pos = (posActXTem, posActYTem-1)
+            item = ((pos, padre), aux)
+            if (self.poblacionRe.count(item) == 0):
+                state = True
+                self.poblacion.append(item)
+        return state
+
+    def seleccionar(self):
+        mini = 10000000000000
+        for unity in self.poblacion:
+            if (mini >= unity[1]):
+                mini = unity[1]
+                minEl = unity
+        self.poblacion.remove(minEl)
+        self.poblacionRe.append(minEl)
+        return minEl
+
+    def think(self, env):
+        self.cont = 0
+        aux = env.functioH(env.posInitX, env.posInitY)
+        item = (env.posInitX, env.posInitY)
+        parent = (None, None)
+        self.poblacionRe.append(((item, parent), aux))
+        self.comprobarFrontera(env, env.posInitX, env.posInitY)
+        if (len(self.poblacion) > 0):
+            poblacionFin = []
+            aux = self.think2(env, poblacionFin)
+            return self.show(aux)
+        else:
+            print("Sin resultado")
+            return []
+
+    def think2(self, env, poblacionFin):
+        self.cont = self.cont+1
+        (((posX, posY), (parentX, parentY)), costH) = self.seleccionar()
+        self.comprobarFrontera(env, posX, posY)
+        poblacionFin.append(((posX, posY), (parentX, parentY)))
+        if (costH == 0):
+            print("Resultado")
+            return poblacionFin
+        if (len(self.poblacion) == 0):
+            print("Sin resultado")
+            return []
+        return self.think2(env, poblacionFin)
+
+    def show(self, pob):
+        rev = []
+        aux = reversed(pob)
+        for unity in aux:
+            rev.append(unity[1])
+        return reversed(rev)
+
+    def printF(self, env, pob):
+        print(self.cont)
+        env.print_environment(env.posInitX, env.posInitY)
+        for unity in pob:
+            time.sleep(0.6)
+            env.print_environment(unity[0], unity[1])
+
+    def getPoblacion(self):
+        return self.poblacion
--- TP_3/main.py
+++ TP_3/main.py
@@ -1,13 +1,14 @@
 # https://docs.google.com/document/d/1I-4UuCsW-cr8ttmY0VJ82_s-EDc_nKbG6IoroZT_Uq4/edit
 
 from agentThink import AgentE
+from agentThinkTest import AgentETest
 from environment import Environment
 
-env1 = Environment(18, 18, 0.3)
+env1 = Environment(10, 10, 0.3)
 
-agenE = AgentE(env1)
+#agenE = AgentE(env1)
+agenE = AgentETest(env1)
 aux = agenE.think(env1)
 agenE.printF(env1, aux)
-
 
 print("______________________________________________________________________________________")
